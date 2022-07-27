ba75e7ee262dea05ab3093797f5018fb20c06628
egan918 <lo.egan918@gmail.com>
2022-07-27 15:24:27 +0900

    updated

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Navbar from "../components/navbar"
+import Home from "./Home"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Home />
+      </main>
+    </>
+  )
+}
+
+export default App
